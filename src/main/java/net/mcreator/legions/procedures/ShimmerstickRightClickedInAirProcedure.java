be2f4ccdc72b73b18eda1bab153e2abcaf731c54
noobmaster67 <15735@DESKTOP-PMPO01O.T-mobile.com>
2021-04-16 06:34:37 -0500

    GOSH DANG TICKS

--- src/main/java/net/mcreator/legions/procedures/ShimmerstickRightClickedInAirProcedure.java
+++ src/main/java/net/mcreator/legions/procedures/ShimmerstickRightClickedInAirProcedure.java
@@ -23,10 +23,10 @@ public class ShimmerstickRightClickedInAirProcedure extends LegionsModElements.M
 		}
 		Entity entity = (Entity) dependencies.get("entity");
 		if (entity instanceof LivingEntity)
-			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, (int) 60, (int) 1));
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, (int) 1000, (int) 2));
 		if (entity instanceof LivingEntity)
-			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 60, (int) 1));
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 1000, (int) 2));
 		if (entity instanceof LivingEntity)
-			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.INVISIBILITY, (int) 60, (int) 1));
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.INVISIBILITY, (int) 1000, (int) 1));
 	}
 }
