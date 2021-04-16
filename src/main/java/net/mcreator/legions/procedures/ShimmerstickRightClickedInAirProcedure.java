50e98672fb611d6268f8b49a3a4904920ede271b
noobmaster67 <15735@DESKTOP-PMPO01O.T-mobile.com>
2021-04-16 06:28:05 -0500

    e

---
+++ src/main/java/net/mcreator/legions/procedures/ShimmerstickRightClickedInAirProcedure.java
@@ -0,0 +1,32 @@
+package net.mcreator.legions.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.legions.LegionsModElements;
+
+import java.util.Map;
+
+@LegionsModElements.ModElement.Tag
+public class ShimmerstickRightClickedInAirProcedure extends LegionsModElements.ModElement {
+	public ShimmerstickRightClickedInAirProcedure(LegionsModElements instance) {
+		super(instance, 1);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure ShimmerstickRightClickedInAir!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, (int) 16, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 16, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.INVISIBILITY, (int) 16, (int) 1));
+	}
+}
